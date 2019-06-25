2c54dd6b99ddc0a6365b446a89954502c2e45bf7
vla22 <vla22@cam.ac.uk>
2019-06-25 10:57:53 +0100

    Expanded BDD test using .feature file [ORCA-5]

--- src/tango_sdp_subarray/tests/test_SDP_Subarray.py
+++ src/tango_sdp_subarray/tests/test_SDP_Subarray.py
@@ -1,4 +1,15 @@
 from pytest_bdd import scenario, given, when, then
 
-@scenario(
-def 
+@scenario('1_XR-7.feature', 'Scanning')
+def test_scanning():
+    pass
+
+@given('a Ready SDP State Device')
+def test_device():
+    device = SDP_Subarray.init_device()
+    #I know this won't really set up a suitable device, but...
+
+@when('the SDP State Device recieves the Run command')
+
+
+@then('the SDP State Device will transition to the Scanning State')
