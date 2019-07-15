e8235dbd0a1a88e4d03559fd7fd1da93f7eb3c86
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2019-07-15 15:07:44 +0100

    ORCA-94 Removed tools directory and moved test scripts to scripts folder

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 [![pipeline status](https://gitlab.com/ska-telescope/sdp-prototype/badges/master/pipeline.svg)](https://gitlab.com/ska-telescope/sdp-prototype/pipelines)
 [![Documentation Status](https://readthedocs.org/projects/sdp-configuration-prototype/badge/?version=latest)](https://sdp-configuration-prototype.readthedocs.io/en/latest/?badge=latest)
-[![coverage](https://gitlab.com/ska-telescope/sdp-prototype/badges/master/coverage.svg)](https://ska-telescope.gitlab.io/sdp-prototype/htmlcov/)
+[![coverage](https://gitlab.com/ska-telescope/sdp-prototype/badges/master/coverage.svg)](https://sdp-configuration-prototype.readthedocs.io/htmlcov/)
 # SDP Evolutionary Prototype
 
 Prototype of minimal SDP components required for configuration and execution of
--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,57 +1,67 @@
-#!/usr/bin/env bash
-RED='\033[0;31m'
-NC='\033[0m'
+#!/bin/bash
+# Run tests and linters for specified directory.
+#
+# Usage:
+#   ./tools/run_tests.sh <directory> [--test-only] [pytest flags]
+#
+# Example:
+#   ./tools/run_tests.sh sip/execution_control/configuration_db \
+#        --test-only -x -k test_workflow_definitions
+#
 
-DIR="$1"
-OPTIONS=("${@:2}")
+Usage() {
+   printf "Usage: %s <directory> [--test-only] [pytest flags]\n" "$0"
+   exit 1
+}
 
-if [[ -n "${OPTIONS[*]}" && "${OPTIONS[0]}" == "--tests-only" ]]; then
-    OPTIONS=("${OPTIONS[@]:1}")
-    CMD="python3 -m pytest -s -vv \
-        --rootdir=. \
-        ${OPTIONS[*]} \
-        ${DIR}"
-elif [[ -n "${OPTIONS[*]}" && "${OPTIONS[0]}" == "--no-cov" ]]; then
-    OPTIONS=("${OPTIONS[@]:1}")
-    CMD="python3 -m pytest -s -vv \
-        --rootdir=. \
-        --pylint \
-        --pylint-rcfile=./scripts/.pylintrc \
-        --codestyle \
-        --docstyle \
-        ${OPTIONS[*]} \
-        ${DIR}"
-elif [[ -n "${OPTIONS[*]}" ]]; then
-    CMD="python3 -m pytest -s -vv \
-        --rootdir=. \
-        --pylint \
-        --pylint-rcfile=./scripts/.pylintrc \
-        --codestyle \
-        --docstyle \
-        --cov-config=./scripts/setup.cfg \
-        --cov-append \
-        --cov-branch \
-        --no-cov-on-fail \
-        --cov=${DIR} \
-        ${OPTIONS[*]} \
-        ${DIR}"
+# Colours used in printing output
+RED=$(tput setaf 1)
+BLUE=$(tput setaf 4)
+NC=$(tput sgr0)
+
+
+[[ -n $1 ]] && [[ -d $1 ]] || {
+   printf "directory not provided or does not exist\n"
+   Usage
+}
+
+DIR=$1
+shift
+printf "%s* DIR=%s'%s'\n" "$RED" "$NC" "$DIR"
+
+if [[ -n $1 ]] && [[ $1 = --test-only ]]
+then
+    shift
+    full_test_options=
 else
-    CMD="python3 -m pytest -s -vv \
-        --rootdir=. \
-        --pylint \
-        --pylint-rcfile=./scripts/.pylintrc \
-        --codestyle \
-        --docstyle \
-        --cov-config=./scripts/setup.cfg \
-        --cov-append \
-        --cov-branch \
-        --no-cov-on-fail \
-        --cov=${DIR} \
-        ${DIR}"
+    full_test_options="
+    --pylint
+    --pylint-rcfile=.pylintrc
+    --codestyle
+    --docstyle
+    --cov-config=./scripts/setup.cfg \
+    --cov=${DIR}
+    --cov-report=term
+    --cov-report=html
+    "
 fi
 
+if [[ $# -ge 1 ]]
+then
+    prefix_options=$'-m pytest -s -v \
+    --rootdir=.'
+    printf "%s------------------------------------------------------\n" \
+            "$RED"
+    printf "* OPTIONS=%s%s\n" "$NC" "$*"
+else
+     prefix_options=$'-m pytest -s -vv
+    --rootdir=.'
+fi
+printf "%s------------------------------------------------------\n" \
+            "$RED"
+printf "%sRunning tests:\n   python3 %s %s %s %s\n" \
+             "$BLUE" "$prefix_options" "$full_test_options" "$*" "$DIR"
+printf "%s------------------------------------------------------%s\n" \
+            "$RED" "$NC"
 
-echo -e "${RED}---------------------------------------${NC}"
-echo -e "${CMD}"
-echo -e "${RED}---------------------------------------${NC}"
-eval "${CMD}"
+python3 $prefix_options $full_test_options $* $DIR
