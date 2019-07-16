d8f7d9a50383f288b4b4d505cc8ac4cb158f182a
Andrew McDermott <andrew.mcdermott@stfc.ac.uk>
2019-07-16 14:01:50 +0100

    Initial version of run_c_tests.sh based on SKA/SDP version

---
+++ scripts/run_c_tests.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+cd sdp-prototype/src/vis_receive
+[[ -e ./build ]] && printf "\n**** removing old build directory ****\n" && rm -rf ./build
+echo -e "\n ${bold}*** Running Cpp Check *** ${normal} \n"
+cppcheck ./ -i extern/gtest/ --enable=warning,portability,style
+mkdir ./build
+cd ./build
+
+echo -e "\n ${bold}*** Running Coveralls *** ${normal} \n"
+if cmake -DCOVERALLS=ON -DCMAKE_BUILD_TYPE=Debug .. 
+then
+   make
+   make coveralls
+
+   echo -e "\n ${bold}*** Running Valgrind using Ctest testing tool *** ${normal} \n"
+   ctest -T memcheck
+else
+   echo -e "\n ${bold}*** Unable to run Coveralls or Valgrind *** ${normal} \n"
+fi
+
+echo -e "\n ${bold}*** Running Undefined Behaviour Sanitizer *** ${normal} \n"
+#cd .. && rm -r -f build && mkdir build && cd build
+rm -rf *
+cmake -DENABLE_USAN=ON ..
+make
+./tests/recv_test
+
+echo -e "\n ${bold}*** Running Threading Sanitizer *** ${normal} \n"
+#cd .. && rm -r -f build && mkdir build && cd build
+rm -rf *
+cmake -DENABLE_TSAN=ON ..
+make
+./tests/recv_test
+
+echo -e "\n ${bold}*** Running Address Sanitizer *** ${normal} \n"
+#cd .. && rm -r -f build && mkdir build && cd build
+rm -rf *
+cmake -DENABLE_ASAN=ON ..
+make
+./tests/recv_test
+
