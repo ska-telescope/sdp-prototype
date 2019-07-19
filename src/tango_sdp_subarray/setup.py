71dc81c1ff2512072e15ee066892f08f13850ac2
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2019-07-19 13:24:09 +0100

    ORCA-93 Released a new version of package 0.3.0 and fixed pylint errors

--- src/tango_sdp_subarray/setup.py
+++ src/tango_sdp_subarray/setup.py
@@ -3,23 +3,28 @@
 """PIP setup script for the SDP Subarray Device package."""
 import os
 from setuptools import setup
+# pylint: disable=invalid-name, exec-used, undefined-variable
 
 setup_dir = os.path.dirname(os.path.abspath(__file__))
 
 release_filename = os.path.join(setup_dir, 'SDPSubarray', 'release.py')
 exec(open(release_filename).read())
 
+with open('README.md', 'r') as file:
+    LONG_DESCRIPTION = file.read()
+
 setup(
     name=NAME,
     version=VERSION,
     description='SKA SDP Subarray',
-    long_description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
     author=AUTHOR,
-    url=
-    'https://github.com/ska-telescope/sdp-prototype/src/tango_sdp_subarray',
+    url='https://github.com/ska-telescope/sdp-prototype/src/'
+        'tango_sdp_subarray',
     classifiers=[
-          "Programming Language :: Python :: 3 :: Only",
-          "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3 :: Only",
+        "Development Status :: 1 - Planning",
     ],
     license=LICENSE,
     packages=['SDPSubarray'],
