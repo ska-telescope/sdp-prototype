f99777cf0aa0ff2f7843c0e34858aa8f8abd9bf0
Peter Wortmann <pw410@cam.ac.uk>
2019-07-22 10:59:48 +0100

    Fix etcd script
    Might now actually do what it says!

--- examples/docker_run_etcd.sh
+++ examples/docker_run_etcd.sh
@@ -11,13 +11,13 @@ docker run -d -h etcd --name etcd \
     --initial-cluster=default=http://etcd:2380 $* || exit 1
 
 ETCDCTL_ENDPOINTS=`docker port etcd 2379`
-SDP_CONFIG_HOST=`echo $ETCDCTL_ENDPOINTS | sed 's/.*://'`
-SDP_CONFIG_PORT=`echo $ETCDCTL_ENDPOINTS | sed 's/:.*//'`
+SDP_CONFIG_PORT=`echo $ETCDCTL_ENDPOINTS | sed 's/.*://'`
+SDP_CONFIG_HOST=`echo $ETCDCTL_ENDPOINTS | sed 's/:.*//'`
 
 echo
 echo Etcd container is running. Test by going:
 echo  $ docker run --link etcd --env ETCDCTL_ENDPOINTS=etcd:2379 --env ETCDCTL_API=3 -i quay.io/coreos/etcd:latest /usr/local/bin/etcdctl endpoint health
-echo  $ ETCDCTL_ENDPOINTS=$ETCD_LOCAL ETCDCTL_API=3 etcdctl endpoint health
+echo  $ ETCDCTL_ENDPOINTS=$ETCDCTL_ENDPOINTS ETCDCTL_API=3 etcdctl endpoint health
 echo "(use second version if you have etcdctl installed locally)"
 echo
 echo To use with ska-sdp-config, either link into container, or use locally:
