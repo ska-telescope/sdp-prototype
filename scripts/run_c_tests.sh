a508098427677ca320105c8ad997a21cb44870d9
Andrew McDermott <andrew.mcdermott@stfc.ac.uk>
2019-08-02 13:32:10 +0100

    ORCA-104: Will this run the CI tests in parallel?

--- scripts/run_c_tests.sh
+++ scripts/run_c_tests.sh
@@ -1,50 +1,60 @@
 #!/bin/bash
 
+# Runs one test at a time.
+# Usage: run_c_tests.sh test
+# Where test is one of:
+#   - CPP (CPPCheck, CPP-check ...) for the CPP check
+#   - USAN (undefinded, behaviour, ...) for the undefined behaviour check
+#   - TSAN (thread, ...) for the thread sanity check
+#   - ASAN (address ...) for the address sanity check
+#   - COV (COVERALLS) Coveralls check
+# The test argument can be upper or lower case.
+# There are aliases for the sanity checks.
+
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-#temp hack
-cd app
+# Is this a better hack?
+[[ -d /app ]] && cd /app
 
 cd src/vis_receive
-[[ -e ./build ]] && printf "\n**** removing old build directory ****\n" && rm -rf ./build
-echo -e "\n ${bold}*** Running Cpp Check *** ${normal} \n"
-cppcheck ./ -i extern/gtest/ --enable=warning,portability,style
+[[ -e ./build ]] && printf "\n**** deleting old build directory ****\n" && \
+	rm -rf ./build
+
 mkdir ./build
 cd ./build
-
-echo -e "\n ${bold}*** Running Undefined Behaviour Sanitizer *** ${normal} \n"
-#cd .. && rm -r -f build && mkdir build && cd build
-rm -rf *
-cmake -DENABLE_USAN=ON ..
-make
-./tests/recv_test
-
-echo -e "\n ${bold}*** Running Threading Sanitizer *** ${normal} \n"
-#cd .. && rm -r -f build && mkdir build && cd build
-rm -rf *
-cmake -DENABLE_TSAN=ON ..
-make
-./tests/recv_test
-
-echo -e "\n ${bold}*** Running Address Sanitizer *** ${normal} \n"
-#cd .. && rm -r -f build && mkdir build && cd build
-rm -rf *
-cmake -DENABLE_ASAN=ON ..
-make
-./tests/recv_test
-
-rm -rf *
-cmake -DCOVERALLS=ON -DCMAKE_BUILD_TYPE=Debug ..
-
-echo -e "\n ${bold}*** Running Coveralls *** ${normal} \n"
-if cmake -DCOVERALLS=ON -DCMAKE_BUILD_TYPE=Debug ..
-then
-   make
-   make coveralls
-
-   # echo -e "\n ${bold}*** Running Valgrind using Ctest testing tool *** ${normal} \n"
-   # ctest -T memcheck
-else
-   echo -e "\n ${bold}*** Unable to run Coveralls or Valgrind *** ${normal} \n"
-fi
+case ${1^^} in
+    CPP*)
+	echo -e "\n ${bold}*** Running Cpp Check *** ${normal} \n"
+	cd ..
+	cppcheck ./ -i extern/gtest/ --enable=warning,portability,style
+	cd build
+	;;
+    USAN|UNDEF*|BEHAV*)
+	echo -e "\n ${bold}*** Running Undefined Behaviour Sanitizer *** ${normal} \n"
+	cmake -DENABLE_USAN=ON ..
+	make
+	./tests/recv_test
+	;;
+    TSAN|THREAD*)
+	echo -e "\n ${bold}*** Running Threading Sanitizer *** ${normal} \n"
+	cmake -DENABLE_TSAN=ON ..
+	make
+	./tests/recv_test
+	;;
+    ASAN|ADDRESS*)
+	echo -e "\n ${bold}*** Running Address Sanitizer *** ${normal} \n"
+	cmake -DENABLE_ASAN=ON ..
+	make
+	./tests/recv_test
+	;;
+    COV*)
+	echo -e "\n ${bold}*** Running Coveralls *** ${normal} \n"
+	cmake -DCOVERALLS=ON -DCMAKE_BUILD_TYPE=Debug ..
+	make
+	make coveralls
+	;;
+esac
+cd ..
+printf "\n**** No longer require build directory  - deleting ****\n" && \
+        rm -rf ./build
