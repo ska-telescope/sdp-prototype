6b67d6592311c046fef8bdea2b2a3eaeef41cda9
Benjamin Mort <bmort@users.noreply.github.com>
2019-08-07 12:28:16 +0100

    Removed version property from SDPSubarray.
    This is a quick fix to address an issue with relative imports until
    it can be addressed properly.
    
    ORCA-106

--- src/tango_sdp_subarray/tests/conftest.py
+++ src/tango_sdp_subarray/tests/conftest.py
@@ -7,7 +7,6 @@ import pytest
 from tango.test_context import DeviceTestContext
 
 from SDPSubarray import SDPSubarray
-from SDPSubarray.release import VERSION
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -15,10 +14,7 @@ def tango_context():
     """Fixture that creates SDPSubarray DeviceTestContext object."""
     # pylint: disable=redefined-outer-name
     device_name = 'mid_sdp/elt/subarray_1'
-    properties = dict(version=VERSION)
-    tango_context = DeviceTestContext(SDPSubarray,
-                                      device_name=device_name,
-                                      properties=properties)
+    tango_context = DeviceTestContext(SDPSubarray, device_name=device_name)
     tango_context.start()
     SDPSubarray.get_name = Mock(side_effect=tango_context.get_device_access)
     yield tango_context
