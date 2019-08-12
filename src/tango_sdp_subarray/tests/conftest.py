bd76b8b8856a1456a82d00e9bde8b2bae3fec808
Benjamin Mort <bmort@users.noreply.github.com>
2019-08-12 10:12:02 +0100

    Add functionality to SDPSubarray device receiveAddresses attribute [ORCA-106] (#45)
    Add receiveAddresses functionality to SDPSubarray.
    
    This reads a channel - FSP link map from a CSP subarray and generates a JSON mapping for the receiveAddresses according to the descriptions found at
    <https://confluence.skatelescope.org/pages/viewpage.action?pageId=74716479>
    and
    <https://confluence.skatelescope.org/display/SE/Mid.CBF+Output+Links+and+Destination+Addresses+JSON+Schema>
    
    Added a feature toggle attribute to read or ignore then cbfOutLink
    CSP Subarray attribute. If set to False this populates receiveAddresses
    with a basic hardcoded test response.

--- src/tango_sdp_subarray/tests/conftest.py
+++ src/tango_sdp_subarray/tests/conftest.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 """Pytest plugins."""
 
+from unittest.mock import Mock
+
 import pytest
 from tango.test_context import DeviceTestContext
 
@@ -11,7 +13,9 @@ from SDPSubarray import SDPSubarray
 def tango_context():
     """Fixture that creates SDPSubarray DeviceTestContext object."""
     # pylint: disable=redefined-outer-name
-    tango_context = DeviceTestContext(SDPSubarray)
+    device_name = 'mid_sdp/elt/subarray_1'
+    tango_context = DeviceTestContext(SDPSubarray, device_name=device_name)
     tango_context.start()
+    SDPSubarray.get_name = Mock(side_effect=tango_context.get_device_access)
     yield tango_context
     tango_context.stop()
