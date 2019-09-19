4b8ae22912010b5db2c0518fbb58c9eeb5f16ae8
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2019-09-19 14:01:17 +0100

    SIM-129 updated the unit test

--- src/tango_sdp_subarray/tests/test_subarray_device.py
+++ src/tango_sdp_subarray/tests/test_subarray_device.py
@@ -167,12 +167,8 @@ def command_configure_invalid_json(subarray_device):
 
     :param subarray_device: An SDPSubarray device.
     """
-    # with pytest.raises(tango.DevFailed):
-    #     subarray_device.Configure('{}')
-
-    subarray_device.Configure('{}')
-
-
+    with pytest.raises(tango.DevFailed):
+        subarray_device.Configure('{}')
 
 @when('I call ConfigureScan')
 def command_configure_scan(subarray_device):
