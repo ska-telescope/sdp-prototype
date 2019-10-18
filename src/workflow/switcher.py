4f99415a64379662fd3a821a3c3600baff21a6c3
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2019-10-18 20:51:39 +0000

    SIM-203 Dynamic workflow deployment

---
+++ src/workflow/switcher.py
@@ -0,0 +1,61 @@
+"""
+Workflow switcher
+
+Launches workflows from processing blocks in the configuration database.
+"""
+
+# pylint: disable=C0103
+
+import subprocess
+import logging
+import ska_sdp_config
+
+# Dictionary defining mapping from workflow IDs to Python scripts.
+workflows_realtime = {
+    'testdeploy': 'testdeploy.py',
+    'testdask': 'testdask.py',
+    'vis_receive': 'test_vis_receive.py'
+}
+
+
+def main():
+    """Main loop."""
+
+    logging.basicConfig()
+    log = logging.getLogger('main')
+    log.setLevel(logging.INFO)
+
+    # Connect to configuration database.
+    client = ska_sdp_config.Config()
+
+    log.info("Waiting for processing block...")
+    for txn in client.txn():
+        target_pb_blocks = txn.list_processing_blocks()
+        for pb_id in target_pb_blocks:
+            if txn.get_processing_block_owner(pb_id) is not None:
+                # Processing block is claimed, so continue to the next one.
+                continue
+            pb = txn.get_processing_block(pb_id)
+            wf_type = pb.workflow['type']
+            wf_id = pb.workflow['id']
+            log.info("Found unclaimed PB with workflow of type {0} and id {1}".format(wf_type, wf_id))
+            if wf_type == "realtime":
+                if wf_id in workflows_realtime:
+                    # Spawn Python process with workflow script.
+                    log.info("Launching realtime workflow with id {0}".format(wf_id))
+                    wf_script = workflows_realtime[wf_id]
+                    # TODO: store return value and check for errors.
+                    subprocess.Popen(["python3", wf_script, pb_id])
+                else:
+                    # Unknown realtime workflow ID.
+                    log.error("Unknown realtime workflow id: {0}".format(wf_id))
+            elif wf_type == "batch":
+                log.warning("Batch workflows are not handled at present")
+            else:
+                log.error("Unknown workflow type: {0}".format(wf_type))
+        log.info("Continue waiting...")
+        txn.loop(wait=True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/workflow/test_vis_receive.py
@@ -0,0 +1,57 @@
+"""
+Example Vis Receive workflow
+"""
+
+# pylint: disable=C0103
+
+import logging
+import ska_sdp_config
+import sys
+
+# Initialise logging and configuration
+logging.basicConfig()
+log = logging.getLogger('vis_recv')
+log.setLevel(logging.INFO)
+config = ska_sdp_config.Config()
+
+
+def main(argv):
+    pb_id = argv[0]
+    for txn in config.txn():
+        pb = txn.get_processing_block(pb_id)
+        if txn.get_processing_block_owner(pb_id) is None:
+            # Take ownership
+            txn.take_processing_block(pb_id, config.client_lease)
+
+    # Show
+    log.info("Claimed processing block %s", pb)
+
+    # Deploy Vis Receive with 1 worker.
+    log.info("Deploying Vis Receive...")
+    deploy_id = pb.pb_id + "-vis-receive"
+    deploy = ska_sdp_config.Deployment(
+        deploy_id, "helm", {
+            'chart': 'vis-receive',  # Helm chart deploy/charts/vis-receive
+        })
+    for txn in config.txn():
+        txn.create_deployment(deploy)
+    try:
+
+        # Just idle until processing block or disappears
+        log.info("Done, now idling...")
+        for txn in config.txn():
+            if not txn.is_processing_block_owner(pb.pb_id):
+                break
+            txn.loop(True)
+
+    finally:
+
+        # Clean up vis receive deployment.
+        for txn in config.txn():
+            txn.delete_deployment(deploy)
+
+        config.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
