2b4711ac778b6ec73f6570aee5a6c1981cc32757
Steve Guest <steve.guest@stfc.ac.uk>
2020-04-27 13:22:48 +0100

    add all packages to linting

--- src/processing_controller/test/workflows_test.py
+++ src/processing_controller/test/workflows_test.py
@@ -3,8 +3,10 @@ from pathlib import Path
 from processing_controller import processing_controller
 
 LOG = logging.getLogger(__name__)
-ROOT = str(Path(processing_controller.__file__).parent)
-
+PRJ_ROOT = str(Path(processing_controller.__file__).parent)
+SRC_ROOT = str(Path(processing_controller.__file__).parent.parent.parent)
+SDP_ROOT = str(Path(processing_controller.__file__).parent.parent)
+SCHEMA = PRJ_ROOT+"/schema/workflows.json"
 
 def test_without_json():
     wf = processing_controller.Workflows("not_there.json")
@@ -12,10 +14,22 @@ def test_without_json():
 
 
 def test_bad_json():
-    wf = processing_controller.Workflows(ROOT+"/workflows.py")
+    wf = processing_controller.Workflows(PRJ_ROOT+"/workflows.py")
     assert wf.version == {}
 
 
 def test_with_json():
-    wf = processing_controller.Workflows(ROOT+"/schema/workflows.json")
+    wf = processing_controller.Workflows(SCHEMA)
     assert wf.version == {}
+
+
+def test_scan():
+    wf = processing_controller.Workflows(SCHEMA)
+    wf.update_file(SDP_ROOT+"/examples/scan.json")
+    assert wf.version == {}
+
+
+def test_workflow():
+    wf = processing_controller.Workflows(SCHEMA)
+    wf.update_file(SRC_ROOT+"/workflows/workflows.json")
+    assert wf.version['date-time'].startswith('2019')
