e7bb7f27a4275abbb2222cf646b14b5ea14efd4d
Mark Ashdown <maja1@mrao.cam.ac.uk>
2020-08-06 14:11:02 +0100

    SIM-535 - Parameterise commands in master tests

--- src/lmc/tests/features/master.feature
+++ src/lmc/tests/features/master.feature
@@ -6,50 +6,35 @@ Feature: SDP Master Device
         Then the state should be STANDBY
         And healthState should be OK
 
-    Scenario Outline: On command succeeds
+    Scenario Outline: Command succeeds
         Given I have an SDPMaster device
-        When the state is <state_value>
-        And I call On
-        Then the state should be ON
+        When the state is <initial_state>
+        And I call <command>
+        Then the state should be <final_state>
 
         Examples:
-        |state_value|
-        |OFF        |
-        |STANDBY    |
-        |DISABLE    |
+        |command|initial_state|final_state|
+        |Off    |STANDBY      |OFF        |
+        |Off    |DISABLE      |OFF        |
+        |Off    |ON           |OFF        |
+        |Standby|OFF          |STANDBY    |
+        |Standby|DISABLE      |STANDBY    |
+        |Standby|ON           |STANDBY    |
+        |Disable|OFF          |DISABLE    |
+        |Disable|STANDBY      |DISABLE    |
+        |Disable|ON           |DISABLE    |
+        |On     |OFF          |ON         |
+        |On     |STANDBY      |ON         |
+        |On     |DISABLE      |ON         |
 
-    Scenario Outline: Disable command succeeds
+    Scenario Outline: Command is rejected in disallowed state
         Given I have an SDPMaster device
-        When the state is <state_value>
-        And I call Disable
-        Then the state should be DISABLE
+        When the state is <initial_state>
+        Then calling <command> should raise tango.DevFailed
 
         Examples:
-        |state_value|
-        |OFF        |
-        |STANDBY    |
-        |ON         |
-
-    Scenario Outline: Standby command succeeds
-        Given I have an SDPMaster device
-        When the state is <state_value>
-        And I call Standby
-        Then the state should be STANDBY
-
-        Examples:
-        |state_value|
-        |OFF        |
-        |DISABLE    |
-        |ON         |
-
-    Scenario Outline: Off command succeeds
-        Given I have an SDPMaster device
-        When the state is <state_value>
-        And I call Off
-        Then the state should be OFF
-
-        Examples:
-        |state_value|
-        |STANDBY    |
-        |DISABLE    |
-        |ON         |
+        |command|initial_state|
+        |Off    |OFF          |
+        |Standby|STANDBY      |
+        |Disable|DISABLE      |
+        |On     |ON           |
--- src/lmc/tests/test_master.py
+++ src/lmc/tests/test_master.py
@@ -3,9 +3,10 @@
 # pylint: disable=redefined-outer-name
 # pylint: disable=duplicate-code
 
+import pytest
 from pytest_bdd import (given, parsers, scenarios, then, when)
 
-from tango import DevState
+import tango
 
 from ska_sdp_lmc import HealthState
 
@@ -46,9 +47,9 @@ def initialise_device(master_device):
     master_device.Init()
 
 
-@when(parsers.parse('the state is {state_value}'))
-@when('the state is <state_value>')
-def set_device_state(master_device, state_value):
+@when(parsers.parse('the state is {initial_state:S}'))
+@when('the state is <initial_state>')
+def set_device_state(master_device, initial_state):
     """Set the device state.
 
     :param master_device: SDPMaster device
@@ -57,52 +58,53 @@ def set_device_state(master_device, state_value):
     """
     current_state = master_device.state().name
 
-    if state_value == 'OFF' and current_state != 'OFF':
-        master_device.Off()
-    elif state_value == 'STANDBY' and current_state != 'STANDBY':
-        master_device.Standby()
-    elif state_value == 'DISABLE' and current_state != 'DISABLE':
-        master_device.Disable()
-    elif state_value == 'ON' and current_state != 'ON':
-        master_device.On()
+    if initial_state == 'OFF' and current_state != 'OFF':
+        command(master_device, 'Off')
+    elif initial_state == 'STANDBY' and current_state != 'STANDBY':
+        command(master_device, 'Standby')
+    elif initial_state == 'DISABLE' and current_state != 'DISABLE':
+        command(master_device, 'Disable')
+    elif initial_state == 'ON' and current_state != 'ON':
+        command(master_device, 'On')
 
-    assert master_device.state() == DevState.names[state_value]
+    assert master_device.state() == tango.DevState.names[initial_state]
 
 
-@when(parsers.parse('I call {command_name}'))
-def command(master_device, command_name):
+@when(parsers.parse('I call {command:S}'))
+@when('I call <command>')
+def command(master_device, command):
     """Call the device commands.
 
     :param master_device: SDPMaster device
-    :param command_name: name of command to call
+    :param command: name of command to call
 
     """
-    if command_name == 'Off':
-        master_device.Off()
-    elif command_name == 'Standby':
-        master_device.Standby()
-    elif command_name == 'Disable':
-        master_device.Disable()
-    elif command_name == 'On':
-        master_device.On()
+    # Check command is present
+    command_list = master_device.get_command_list()
+    assert command in command_list
+    # Get command function
+    command_func = getattr(master_device, command)
+    # Call the command
+    command_func()
 
 
 # ----------
 # Then steps
 # ----------
 
-@then(parsers.parse('the state should be {state_value}'))
-def check_device_state(master_device, state_value):
+@then(parsers.parse('the state should be {final_state:S}'))
+@then('the state should be <final_state>')
+def check_device_state(master_device, final_state):
     """Check the device state.
 
     :param master_device: SDPMaster device
-    :param state_value: expected state value
+    :param final_state: expected state value
 
     """
-    assert master_device.state() == DevState.names[state_value]
+    assert master_device.state() == tango.DevState.names[final_state]
 
 
-@then(parsers.parse('healthState should be {health_state_value}'))
+@then(parsers.parse('healthState should be {health_state_value:S}'))
 def check_health_state(master_device, health_state_value):
     """Check healthState.
 
@@ -111,3 +113,21 @@ def check_health_state(master_device, health_state_value):
 
     """
     assert master_device.healthState == HealthState[health_state_value]
+
+
+@then(parsers.parse('calling {command:S} should raise tango.DevFailed'))
+@then('calling <command> should raise tango.DevFailed')
+def command_raises_dev_failed_error(master_device, command):
+    """Check that calling command raises a tango.DevFailed error.
+
+    :param master_device: An SDPMaster device.
+    :param command: the name of the command.
+    """
+    # Check command is present
+    command_list = master_device.get_command_list()
+    assert command in command_list
+    # Get command function
+    command_func = getattr(master_device, command)
+    with pytest.raises(tango.DevFailed):
+        # Call the command
+        command_func()
