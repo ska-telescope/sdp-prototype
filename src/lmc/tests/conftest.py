deb4e5f33b664cf5e0d52fe6d98458c3c6778871
Mark Ashdown <maja1@mrao.cam.ac.uk>
2020-08-20 17:03:24 +0100

    SIM-545: Update tests to use config DB to set state

--- src/lmc/tests/conftest.py
+++ src/lmc/tests/conftest.py
@@ -11,12 +11,12 @@ from ska_sdp_lmc import SDPMaster, SDPSubarray, subarray
 
 logging.basicConfig(level=logging.DEBUG)
 
-# Turn off the SDP config DB in the subarray by default. This will be
-# overridden if the TOGGLE_CONFIG_DB environment variable is set to 1.
+# Use the config DB memory backend in the subarray. This will be overridden if
+# the TOGGLE_CONFIG_DB environment variable is set to 1.
 subarray.FEATURE_CONFIG_DB.set_default(False)
+# Disable the event loop in the subarray. This will be overridden if the
+# TOGGLE_EVENT_LOOP environment variable is set to 1.
 subarray.FEATURE_EVENT_LOOP.set_default(False)
-# subarray_config.SubarrayConfig.get_receive_addresses =\
-#     Mock(return_value=RECEIVE_ADDRESSES)
 
 # List of devices for the test session
 device_info = [
